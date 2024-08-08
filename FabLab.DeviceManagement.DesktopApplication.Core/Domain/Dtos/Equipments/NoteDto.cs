0b26f233e357297b6230d7ff9ca68060dc590c1f
vanvu0211 <vu34304@gmail.com>
2024-08-08 21:56:37 +0700

    full version

---
+++ FabLab.DeviceManagement.DesktopApplication.Core/Domain/Dtos/Equipments/NoteDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabLab.DeviceManagement.DesktopApplication.Core.Domain.Dtos.Equipments
+{
+    public class NoteDto
+    {
+        public string noteDetail { get; set; }
+        public string equipmentId {  get; set; }
+    }
+}
